f0d6e7bf9643f033c3784f795b21bd8bcae2ede3
Ascent817 <vapradeep817@gmail.com>
2022-11-11 17:05:52 -0600

    Switched to poly-solver

--- c++/particle-life/main.cpp
+++ c++/particle-life/main.cpp
@@ -1,120 +1,72 @@
 #include <SFML/Graphics.hpp>
 #include <tgmath.h>
-#include <iostream>
-#include <random>
 
-using namespace std;
-using namespace sf;
-
-int randomRange(int min, int max) // range : [min, max]
+float polynomial(float x)
 {
-    static bool first = true;
-    if (first)
-    {
-        srand(time(NULL));
-        first = false;
-    }
-    return min + rand() % ((max + 1) - min);
+    return 4 * pow(x, 3) - 3 * pow(x, 2) + x + 1;
+    // x^3 - 3x^2 + x + 1
 }
 
-Vector2f normalize(Vector2f v)
+class Polynomial
 {
-    float length = sqrt((v.x * v.x) + (v.y * v.y));
-    if (length != 0)
-    {
-        return v / length;
-    }
-    else
-    {
-        return v;
-    }
-}
+    public:
+        std::vector<Term> terms;
 
-class Particle
+        Polynomial(std::vector<Term> terms)
+        {
+            this->terms = terms;
+        }
+};
+
+struct Term
 {
-public:
-    int type;
-    int radius;
-    int bounds;
-
-    int coefficients[8];
-
-    Color color;
-    CircleShape shape;
-
-    Vector2f position;
-    Vector2f velocity;
-    Vector2f acceleration;
-
-    Particle()
-    {
-        type = 0;
-        position = Vector2f(randomRange(0, VideoMode::getDesktopMode().width), randomRange(0, VideoMode::getDesktopMode().height));
-        velocity = Vector2f(0, 0);
-        acceleration = Vector2f(0, 0);
-        radius = 10;
-        bounds = 100;
-        color = Color::White;
-        shape = CircleShape(10.f);
-    }
-
-    Particle(int type, Vector2f position, Vector2f velocity, Vector2f acceleration)
-    {
-        type = type;
-        position = position;
-        velocity = velocity;
-        acceleration = acceleration;
-    }
-
-    void update(Particle particles[100])
-    {
-        acceleration = {0, 0};
-        velocity = velocity * 0.98f;
-        velocity += acceleration;
-        velocity = normalize(velocity);
-        position += velocity;
-        updateShape();
-    }
-
-    void applyForce(Vector2f force)
-    {
-        acceleration = force;
-        velocity += acceleration;
-    }
-
-    void updateShape()
-    {
-        shape.setPosition(position.x, position.y);
-    }
+    float coefficient;
+    float exponent;
 };
 
 int main()
 {
-    // Create the main window
-    RenderWindow window(VideoMode(), "Particle Life", Style::Fullscreen);
+    // Create the array with points
+    int points[1000];
 
-    // Populate the particle array
-    Particle particles[100];
-    const int SIZE_OF_ELEMENT = sizeof(particles[0]);
+    // Fill the array with points
+    for (int i = 0; i < 1000; i++)
+    {
+        points[i] = polynomial(i / 100.0f);
+    }
+
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(), "Particle Life", sf::Style::Fullscreen);
 
     while (window.isOpen())
     {
-        Event event;
+        sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == Event::Closed)
+            if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
 
         window.clear();
-        for (int i = 0; i < sizeof(particles) / SIZE_OF_ELEMENT; i++)
+
+        for (int i = 0; i < 1000; i++)
         {
-            // particles[i].applyForce(Vector2f(0, rand() % 1) * 0.001f);
-            particles[i].update(particles);
-            window.draw(particles[i].shape);
+            // Plot point
+            sf::CircleShape circle(1);
+            circle.setPosition(i, points[i]);
+            circle.setFillColor(sf::Color::White);
+            window.draw(circle);
+
+            // Plot x axis
+            sf::Vertex line[] =
+            {
+                sf::Vertex(sf::Vector2f(0, 500)),
+                sf::Vertex(sf::Vector2f(1000, 500))
+            };
         }
+
         window.display();
     }
 
