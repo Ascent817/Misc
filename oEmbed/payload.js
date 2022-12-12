a9bb6ea6b61a8bdd6fe470e6f2b3d35e4cd827e1
Ascent817 <vapradeep817@gmail.com>
2022-12-11 18:24:56 -0600

    418C38GA9C

---
+++ oEmbed/payload.js
@@ -0,0 +1,3 @@
+console.log('payload.js loaded');
+console.log('running in native context');
+alert('This site was found to be vulnerable to a stored cross-site scripting attack. Please notify system administrators immediately.');
