47a0e158f7d0a3b951666b0355e602b82124a658
Ascent817 <vapradeep817@gmail.com>
2023-03-19 19:45:03 -0500

    Add bounds constraint

--- java/deep-learning/src/main/java/genetic/GPanel.java
+++ java/deep-learning/src/main/java/genetic/GPanel.java
@@ -11,17 +11,18 @@ class GPanel extends JPanel implements ActionListener {
     Timer timer;
     double dt = 0.05;
     Ship[] ships;
+    int arenaRadius = 250;
 
     public GPanel() {
         this.setBackground(new Color(33, 33, 33));
 
         this.ships = new Ship[10];
 
-        for(int i = 0; i < ships.length; i++) {
-            ships[i] = new Ship(new Vector2D(Math.random() * 1000, Math.random() * 1000));
+        for (int i = 0; i < ships.length; i++) {
+            ships[i] = new Ship(new Vector2D(this.getWidth() + Math.random() * arenaRadius * 2 - arenaRadius, this.getHeight() + Math.random() * arenaRadius * 2));
         }
 
-        timer = new Timer((int)(dt), this);
+        timer = new Timer((int) (dt), this);
         timer.start();
     }
 
@@ -36,16 +37,40 @@ class GPanel extends JPanel implements ActionListener {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(Color.black);
 
+        int screenWidth = this.getWidth();
+        int screenHeight = this.getHeight();
+        Vector2D screenCenter = new Vector2D(screenWidth / 2, screenHeight / 2);
+
+        g2.fillOval((int) screenCenter.x - arenaRadius, (int) screenCenter.y - arenaRadius, arenaRadius * 2, arenaRadius * 2);
+
         Toolkit.getDefaultToolkit().sync();
         for (int i = 0; i < ships.length; i++) {
             Ship ship = ships[i];
             ship.update(dt);
             ship.display(g2);
-            
 
             // Keep the ship in bounds
-            if (ship.position.subtract(new Vector2D(this.getWidth() / 2, this.getHeight() / 2)).magnitude() > 500) {
-                ship.position = ship.position.multiply(-1);
+            if (ship.position.subtract(screenCenter).magnitude() + ship.radius > arenaRadius) {
+                double error = ship.position.subtract(screenCenter).magnitude() - arenaRadius;
+                Vector2D normal = ship.position.subtract(screenCenter).normalize();
+                double dotProduct = ship.velocity.dot(normal);
+                Vector2D response = normal.multiply((ship.radius) + error);
+
+                if (dotProduct > 0) {
+                    ship.velocity = ship.velocity.subtract(normal.multiply(dotProduct));
+                }
+
+                ship.position = ship.position.subtract(response);
+            }
+
+            // Manage ship collisions
+            for (int j = 0; j < ships.length; j++) {
+                if (i != j) {
+                    Ship otherShip = ships[j];
+
+
+                    
+                }
             }
         }
     }
--- java/deep-learning/src/main/java/genetic/Ship.java
+++ java/deep-learning/src/main/java/genetic/Ship.java
@@ -8,24 +8,28 @@ class Ship extends GameObject {
 
     double rotation = 0.0;
 
-    int diameter = 20;
+    int radius = 20;
     Image shipImage;
-    
+
     public Ship(Vector2D position) {
         super(position);
         this.position = position;
         this.shipImage = Toolkit.getDefaultToolkit().getImage("src/main/resources/ship.png");
-        this.shipImage = shipImage.getScaledInstance(diameter, diameter, 0);
+        this.shipImage = shipImage.getScaledInstance(radius * 2, radius * 2, 0);
     }
 
     public void update(double dt) {
         velocity = velocity.multiply(0.99);
         velocity = velocity.add(acceleration.multiply(dt));
         position = position.add(velocity.multiply(dt));
-        acceleration = new Vector2D(Math.random() * 100 - 50, Math.random() * 100 - 50);
+        acceleration = new Vector2D(0, 0);
+
+        // DEBUG_ONLY
+        acceleration = new Vector2D(Math.random() * 50 - 25, Math.random() * 50 - 25);
     }
 
     public void display(Graphics2D g2) {
-        g2.drawImage(shipImage, (int) this.position.x - shipImage.getWidth(null) / 2, (int) this.position.y - shipImage.getHeight(null) / 2, null);
+        g2.drawImage(shipImage, (int) this.position.x - shipImage.getWidth(null) / 2,
+                (int) this.position.y - shipImage.getHeight(null) / 2, null);
     }
 }
