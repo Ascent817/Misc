eb34cc6e75e58ec2fb1d262b362cc5d7d43cda24
Ascent817 <vapradeep817@gmail.com>
2023-07-31 19:39:38 -0500

    Update repo

--- oEmbed/talk.js
+++ oEmbed/talk.js
@@ -1,9 +1,13 @@
 function formSubmit() {
-    let message = document.getElementById("message").value;
+    let message = document.getElementById("message");
     let display = document.getElementById("display");
-    display.innerHTML = message;
+    display.innerHTML = message.value;
     message.value = "";
+    document.cookie = display.innerHTML;
 }
 
 let button = document.getElementById("post");
 button.addEventListener("click", formSubmit);
+
+let display = document.getElementById("display");
+display.innerHTML = document.cookie;
